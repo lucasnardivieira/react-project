34916d41706d0019cfa2703806a23137adc82e7d
Lucas Nardi <lucas.nardi@weedvision.com.br>
2025-03-29 17:50:33 -0300

    ADD task management functionality with initial tasks list and update Tasks component to display tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import React, { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar React",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar vocabulário",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Estudar matemática",
+      description: "Estudar álgebra",
+      completed: false,
+    },
+  ]);
+
   return (
-    <div>
-      <h1>Gerenciador de tarefas</h1>
-      <Tasks></Tasks>
-      <AddTask></AddTask>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTask></AddTask>
+        <Tasks tasks={tasks}></Tasks>
+      </div>
     </div>
   );
 }
